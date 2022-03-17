27216d481002bdbd57ad8ffa9910dc2bfd4aed5c
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-03-17 13:12:07 -0400

    breakpoint change

--- components/navbar/cheveronMenu.jsx
+++ components/navbar/cheveronMenu.jsx
@@ -1,95 +1,61 @@
-import * as React from "react";
-import Button from "@mui/material/Button";
-import Menu from "@mui/material/Menu";
-import { MenuItemUnstyled, MenuUnstyled } from "@mui/base";
+import React from "react";
+import Link from "../shared/link";
+import { Box } from "@mui/system";
 import { Typography } from "@mui/material";
+import { ButtonUnstyled } from "@mui/base";
+import { AnimatePresence, motion } from "framer-motion";
+import SelectUnstyled from "@mui/base/SelectUnstyled";
 
-export default function BasicMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+export default function CheveronMenu() {
+  const [active, setActive] = React.useState(false);
   return (
-    <div>
-      <Button
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-        sx={{
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          columnGap: "2rem",
-          borderLeft: "2px solid #532A10",
-          bgcolor: "#F5E4D7",
-          px: "8rem",
-        }}
-      >
-        <Typography
-          sx={{ fontSize: "3rem", color: "#532A10", fontWeight: 600 }}
+    <AnimatePresence>
+      <Box>
+        <Box
+          component={ButtonUnstyled}
+          onClick={() => {
+            setActive(!active);
+          }}
+          sx={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            columnGap: "2rem",
+            borderLeft: "2px solid #532A10",
+            bgcolor: "#F5E4D7",
+            px: "8rem",
+          }}
         >
-          Order Now
-        </Typography>
-      </Button>
-      <MenuUnstyled
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItemUnstyled sx={{ width: "100%", bottom: "-200px" }} onClick={handleClose}>
-          Profile
-        </MenuItemUnstyled>
-      </MenuUnstyled>
-    </div>
+          <Typography
+            sx={{ fontSize: "3rem", color: "#532A10", fontWeight: 600 }}
+          >
+            Order Now
+          </Typography>
+          <img src="/cafe_assets/Desktop/Desktop Cheveron.svg"></img>
+        </Box>
+        <Box sx={{ position: "absolute" }}>
+          <Box
+            component={ButtonUnstyled}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              columnGap: "2rem",
+              border: "2px solid #532A10",
+              bgcolor: "#F5E4D7",
+              px: "8rem",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: "3rem", color: "#532A10", fontWeight: 600 }}
+            >
+              Order Now
+            </Typography>
+            <img src="/cafe_assets/Desktop/Desktop Cheveron.svg"></img>
+          </Box>
+        </Box>
+      </Box>
+    </AnimatePresence>
   );
 }
-// import React from "react";
-// import Link from "../shared/link";
-// import { Box } from "@mui/system";
-// import { Typography } from "@mui/material";
-// import { ButtonUnstyled } from "@mui/base";
-// import { AnimatePresence, motion } from "framer-motion";
-// import SelectUnstyled from "@mui/base/SelectUnstyled";
-
-// export default function CheveronMenu() {
-//   const [active, setActive] = React.useState(false);
-//   return (
-//     <AnimatePresence>
-//       <Box
-//         component={SelectUnstyled}
-//         onClick={() => {
-//           setActive(!active);
-//         }}
-//         sx={{
-//           height: "100%",
-//           display: "flex",
-//           justifyContent: "center",
-//           alignItems: "center",
-//           columnGap: "2rem",
-//           borderLeft: "2px solid #532A10",
-//           bgcolor: "#F5E4D7",
-//           px: "8rem",
-//         }}
-//       >
-//         <Typography
-//           sx={{ fontSize: "3rem", color: "#532A10", fontWeight: 600 }}
-//         >
-//           Order Now
-//         </Typography>
-//         <img src="/cafe_assets/Desktop/Desktop Cheveron.svg"></img>
-//       </Box>
-//     </AnimatePresence>
-//   );
-// }
